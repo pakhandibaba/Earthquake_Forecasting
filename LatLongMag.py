68158dfc691755f03b33e2666ae2374df19c86b1
Dhruv Khattar <dhruv@khattarindia.com>
2016-04-11 23:47:58 +0530

    Converted excel to text and modified the code to match Lat Long with its magnitude

--- LatLongMag.py
+++ LatLongMag.py
@@ -1,27 +1,21 @@
 #! /usr/bin/env python
 
-from xlrd import open_workbook
-
-book = open_workbook('Indian-Earthquakes-List-Update_Magnitudes.xls', on_demand=True)
 latlong = open('info.txt','r')
+infile = open('Earthquakes.txt','r')
 outfile = open('LatLongMag.data','w')
 
 values = latlong.read().replace('\n', '').split(',')
+earthquakes = infile.read().split('\n')
 
-sheet = book.sheet_by_name('EQs') 
-
-cnt = 0
 for val in values:
-    print cnt
-    r = 0
     val = val.split()
-    for lat in sheet.col(11):
-        lat = lat.value
-        if float(val[0]) == lat and float(val[1]) == sheet.col(12)[r].value:
-            stri = str(lat) + str(sheet.col(12)[r].value) + str(sheet.col(6)[r].value) + '\n'
-            outfile.write(stri)
-        r = r + 1
-    cnt = cnt+1
+    for row in earthquakes:
+        row = row.split()
+        if len(row) >= 3:
+            if val[0] == row[0] and val[1] == row[1]:
+                stri = row[0] + ' ' + row[1] + ' ' + row[2] + '\n'
+                outfile.write(stri)
 
+infile.close()
 latlong.close()
 outfile.close()
