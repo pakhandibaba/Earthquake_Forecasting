21fbdfbde12f52e142078e6c6d3c8172b4953d92
Dhruv Khattar <dhruv@khattarindia.com>
2016-04-13 22:29:52 +0530

    Year added

--- LatLongMag.py
+++ LatLongMag.py
@@ -7,14 +7,22 @@ outfile = open('LatLongMag.data','w')
 values = latlong.read().replace('\n', '').split(',')
 earthquakes = infile.read().split('\n')
 
+cnt = 0
+
+final = ''
+
 for val in values:
     val = val.split()
     for row in earthquakes:
         row = row.split()
-        if len(row) >= 3:
-            if val[0] == row[0] and val[1] == row[1]:
-                stri = row[0] + '\n' + row[1] + '\n' + row[2] + '\n'
-                outfile.write(stri)
+        if len(row) >= 4:
+            if val[0] == row[1] and val[1] == row[2]:
+                stri = row[1] + ' ' + row[2] + ' ' + row[3] + ' ' + row[0] + '\n'
+                final = final + stri
+                cnt = cnt + 1
+
+outfile.write(str(cnt) + '\n')
+outfile.write(final)
 
 infile.close()
 latlong.close()
